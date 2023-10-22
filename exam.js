b13c96333bdaeb741e13b0054401c7e155dbebb3
MH-Mehedi0 <144585649+MH-Mehedi0@users.noreply.github.com>
2023-10-22 13:33:47 +0600

    Add files via upload

---
+++ exam.js
@@ -0,0 +1,227 @@
+// Exam File Of MD. Mehedi Hasan
+
+// Ans to the ques. No. -01
+
+// var me = " My neme is MD. Mehedi Hasan";
+
+// Ans To the ques. No 03
+
+// Bibinno variable ar Name...
+
+// Data Type                Example
+
+// 1st: string         "Wit Institute"
+// 2nd: Intiger         1,2,3,4,5
+// 3rd: float           1.23, 4.5
+
+// Ans to the question no 04
+
+// ১ঃ ভ্যারিয়েবেল এর নাম অবশ্যই ইউনিক হতে হবে ।
+// ২ঃ ভ্যারিয়েবেল এর নাম রিজার্ব কি ওয়ার্ড হওয়া যাবে নাহ ।
+// ৩ঃ প্রতিটা ভ্যারিয়েবেলের নাম আলাদা আলাদা হতে হবে ।
+
+// Ans to the ques no 5
+
+// var name = " Md. Mehedi Hasan ";
+
+// Ans to the ques no 6 
+
+// Arithmetic Operator  এর কাজ হলো ( যোগ, বিয়োগ, গুন, ভাগ ) করা। 
+
+// Ans to the Ques no 7
+
+// % = শতকরা নির্নয় করার কাজে use হয়।
+// ** = Subscript বের করা যায়।
+
+
+// Ans to the ques No 8
+
+// x + y
+
+// এখানে x হলো Operend ।
+
+// Ans to the Ques No 9
+
+// var x = 10;
+// var y = 36;
+// var z = x + y;
+
+// console.log(z);
+
+// Ans to the ques No 10
+
+// var x = 375;
+// var y = 8;
+// var z = x % y;
+
+// console.log(z);
+
+// Ans to the ques no 11
+    // var x = 8;
+    // var y = x**3;
+    // console.log(y);
+
+// Ans to the Ques No 12
+
+// var math = 80;
+// var chemistry = 82;
+// var physics = 96;
+// var bangla = 76;
+// var totl_sub = 4;
+
+// var total_mark = math + chemistry + physics + bangla;
+// var avg = total_mark / 4;
+
+// console.log(avg);
+
+
+
+
+// Ans to the ques no 13
+
+// var x = 30;
+// var y = '23';
+// var z =parseInt(y);
+// console.log(x+z);
+
+// Ans to the ques no 14
+
+// typeof  দিয়ে Data type খোজে বের করা হয়।
+
+// Ans to the ques No 15
+
+// var num = "55";
+// console.log(parseInt(num));
+
+// Ans to the ques no 16
+//  The index of array is start from = 0;
+
+// Ans to the ques no 17
+
+
+
+// var numbers = [34,56,74,24,88,34,776,34,5];
+// numbers[5] = 334;
+// console.log(numbers);
+
+// Ans to the ques No 18
+
+// var numbers = [34,56,74,24,88,34,776,34,5];
+// numbers.push(234);
+// numbers.unshift(998);
+// console.log(numbers);
+
+// Ans to the ques no 19
+
+// var numbers = [[1,2],[3,4],[5,6]];
+// console.log(numbers.flat());
+
+// Ans to ques no 20
+
+// var team1 = ["rahim", "karim"];
+// var team2 = ["tanim", "biplob", "tareq"];
+// var team3 = ["sajjid", "kaarefinrim"];
+// var full_team = team1.concat(team2,team3);
+
+// console.log(full_team.toString());
+
+// Ans to the Ques No 21
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.shift();
+
+// console.log(fruits);
+
+// Ans to the Ques no 22
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.slice(3,"lichi", "Pineapple", "Watermelon");
+
+// console.log(fruits);
+
+// Ans to the ques no 23
+
+// x = 220; 
+// y = 500;
+
+// if (x>y){
+//     console.log("The Biggest number Is "+ x);
+// }
+// else {
+//     console.log("The Biggest number Is "+ y);
+// }
+
+
+// Ans to the ques no 24
+
+// var x = 20;
+// var y = 10;
+// var Operators = +;
+
+// var result = x + Operators + y;
+
+// console.log(result);
+
+// Ans to the ques no 25
+
+// var numbers_of_pen = 10;
+// var numbers_of_price = 185;
+// var pen_price_of = 3;
+
+// var price_of_single_pen = numbers_of_price / numbers_of_pen;
+// var result = price_of_single_pen * pen_price_of;
+
+// console.log(result);
+
+
+// var book = 500;
+// var percentage = 100;
+// var discount_number = 20;
+
+// var discount =book / percentage;
+// var discount_price = discount*discount_number;
+// console.log(discount_price);
+
+// Ans to the ques no 27**
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// var result = fruits.indexOf("kiwi");
+
+// console.log(result);
+
+// Ans to the ques no 29
+
+// var math = 80;
+// var chemistry = 82;
+// var physics = 96;
+// var bangla = 76;
+// var totl_sub = 4;
+
+// var totel_number = math + chemistry + physics + bangla;
+// var avg_marks = totel_number /  totl_sub;
+
+// if (avg_marks >= 90) {
+//     console.log("The grad is S");
+// }
+// else if (avg_marks >= 80 && avg_marks <= 89) {
+//     console.log("The grad is A");
+// }
+// else if (avg_marks >= 70 && avg_marks <= 79) {
+//     console.log("The grad is B");
+// }
+// else if (avg_marks >= 60 && avg_marks <= 69) {
+//     console.log("The grad is C");
+// }
+// else if (avg_marks >= 50 && avg_marks <= 59) {
+//     console.log("The grad is D");
+// }
+// else if (avg_marks >= 50 && avg_marks <= 59) {
+//     console.log("The grad is E");
+// }
+// else{
+//     console.log("The Student is Failed")
+// }
+
+// Ans to the ques no 30
+
+আমি মোঃ মেহেদী হাসান। আমি Computer Science এর একজন ছাত্র। আমি আমার সময় এর best use করতে পারবো নতুন নতুন স্কিল শিখার মাধ্যমে। আমি Web Design নিয়ে শিখছি। আমি এই বিষয়ে আরো বিস্তর ভাবে Reserch করে আমার সময়ের সঠিক ব্যবহার করতে পারি। তার সাথে সাথে আরো অনেক কিছুই করা যায়। যেমন কিছু Extra curricular activities শিখে সময় কে কাজে লাগানো যায়।
